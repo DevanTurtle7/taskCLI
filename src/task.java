c4bf32e74a780178a8e5f3c7cbcbcaf8a2422565
Devan Kavalchek <devankav7@gmail.com>
2021-02-08 18:52:50 -0500

    stubbed task class

--- src/task.java
+++ src/task.java
@@ -1,5 +1,13 @@
 package src;
 
+import java.util.Date;
+
 public class task {
-    
+    private String name;
+    private Date due;
+
+    public task(String name, Date due) {
+        this.name = name;
+        this.due = due;
+    }
 }
