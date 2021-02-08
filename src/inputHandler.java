4e18163e5b8a55531a49476056037be63503e21c
Devan Kavalchek <devankav7@gmail.com>
2021-02-08 18:00:52 -0500

    added comments

--- src/inputHandler.java
+++ src/inputHandler.java
@@ -6,14 +6,14 @@ public class inputHandler {
      * @param input The text entered by the user
      */
     public static void handleInput(String input) {
-        String[] tokens = input.split(" ");
-        int numArguments = tokens.length;
+        String[] tokens = input.split(" "); // Tokenize the input
+        int numArguments = tokens.length; // Get the number of arguments
 
         if (numArguments == 1) {
-            String command = tokens[0].toLowerCase();
+            String command = tokens[0].toLowerCase(); // Convert the command to lowercase so case doesn't matter
 
             if (command.equals("exit") || command.equals("quit")) {
-                _main.exitProgram();
+                _main.exitProgram(); // Exit the program
             }
         }
     }
