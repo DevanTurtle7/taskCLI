66226fc9645f1fc0f8f9969ba8f6ce7b072ff638
Devan Kavalchek <devankav7@gmail.com>
2021-02-08 21:56:58 -0500

    added comments to memory.java

--- memory/memory.java
+++ memory/memory.java
@@ -8,22 +8,26 @@ import src._main;
 import src.Task;
 
 public class memory {
-    public static Task[] taskMemory = new Task[100];
 
+    public static Task[] taskMemory = new Task[100]; // An array where tasks are stored
+
+    /**
+     * Saves all of the tasks in taskMemory to memory.csv
+     */
     public static void writeMemory() {
         try {
-            FileWriter fileWriter = new FileWriter("memory/memory.csv");
-            fileWriter.write("Name,Date\n");
+            FileWriter fileWriter = new FileWriter("memory/memory.csv"); // Create a new file writer
+            fileWriter.write("Name,Date\n"); // Write the header
 
-            for (Task task : taskMemory) {
-                if (task != null) {
-                    System.out.println("Writing");
-                    fileWriter.write(task.getName() + "," + task.getDue().toString() + "\n");
+            for (Task task : taskMemory) { // Iterate over each task in the memory
+                if (task != null) { // Make sure the current task exists
+                    fileWriter.write(task.getName() + "," + task.getDue().toString() + "\n"); // Write the data
                 }
             }
 
-            fileWriter.close();
+            fileWriter.close(); // Close the file writer
         } catch (IOException e) {
+            // The memory.csv file cannot be read
             System.out.println("An error occurred");
             _main.exitProgram();
         }
@@ -33,18 +37,29 @@ public class memory {
 
     };
 
+    /**
+     * Adds a task to taskMemory
+     * @param task The task being added
+     */
     public static void addTask(Task task) {
+        // Initialize loop variables
         boolean indexFound = false;
+        int i = 0;
+        int memorySize = taskMemory.length;
 
-        for (int i = 0; i < taskMemory.length; i++) {
-            Task index = taskMemory[i];
+        while (!indexFound && i < memorySize) { // Loop until an index is found or the entire array has been checked
+            Task index = taskMemory[i]; // Get the current task
 
-            if (!indexFound) {
-                if (index == null) {
-                    indexFound = true;
-                    taskMemory[i] = task;
-                }
+            if (index == null) { // Check if the index is empty
+                taskMemory[i] = task; // Put the task in the empty index
+                indexFound = true; // Mark indexFound as true
             }
+
+            i++; // Increment i
+        }
+
+        if (!indexFound) { // The array is full
+            System.out.println("Error: The memory is full"); // Print a message
         }
     }
 
